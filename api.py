cbe0f8710061079488f5ccd765ed5db7a9902c7f
Robert Elwell <robert@wikia-inc.com>
2013-08-28 23:06:47 +0000

    added support for coreference counts

--- api.py
+++ api.py
@@ -29,12 +29,44 @@ class ParsedXmlService(restful.Resource):
             response['message'] = 'File not found for document %s' % doc_id
         return response
 
-class AllNounPhrasesDemo(restful.Resource):
+class ParsedJsonService(restful.Resource):
     def get(self, doc_id):
+        response = {}
         xmlResponse = ParsedXmlService().get(doc_id)
         if xmlResponse['status'] != 200:
             return xmlResponse
-        dict = xmltodict.parse(xmlResponse[doc_id])
+        return {'status':200, doc_id: xmltodict.parse(xmlResponse[doc_id])}
+
+class CoreferenceCounts(restful.Resource):
+    def get(self, doc_id):
+        response = {}
+        jsonResponse = ParsedJsonService().get(doc_id)
+        if jsonResponse['status'] != 200:
+            return jsonResponse
+        coreferences = jsonResponse[doc_id]['root']['document']['coreference']['coreference']
+        sentences = jsonResponse[doc_id]['root']['document']['sentences']['sentence']
+        mentionCounts = {}
+        representativeToMentions = {}
+        for coref in coreferences:
+            mentionString = ''
+            mentionCount = 0
+            mentions = []
+            for mention in coref['mention']:
+                mentionCount += 1
+                currentMentionString = " ".join([token['word'] for token in sentences[int(mention['sentence'])-1]['tokens']['token'][int(mention['start'])-1:int(mention['end'])-1]]) 
+                if mention.get('@representative', 'false') == 'true':
+                    mentionString = currentMentionString
+                mentions += [currentMentionString]
+            mentionCounts[mentionString] = mentionCount
+            representativeToMentions[mentionString] = mentions
+        return {doc_id: {'mentionCounts':mentionCounts, 'paraphrases': representativeToMentions}}
+
+class AllNounPhrasesDemo(restful.Resource):
+    def get(self, doc_id):
+        jsonResponse = ParsedJsonService().get(doc_id)
+        if jsonResponse['status'] != 200:
+            return jsonResponse
+        dict = jsonResponse[doc_id]
         nps = []
         for sentence in dict['root']['document']['sentences']['sentence']:
             nps += [' '.join(f.leaves()) for f in nltk.Tree.parse(sentence['parse']).subtrees() if f.node == u'NP']
@@ -71,7 +103,9 @@ class Sentiment(restful.Resource):
         
 
 api.add_resource(ParsedXmlService, '/doc/<string:doc_id>/xml')
+api.add_resource(ParsedJsonService, '/doc/<string:doc_id>/json')
 api.add_resource(AllNounPhrasesDemo, '/doc/<string:doc_id>/nps')
+api.add_resource(CoreferenceCounts, '/doc/<string:doc_id>/corefs')
 api.add_resource(SolrPage, '/doc/<string:doc_id>/solr')
 api.add_resource(Sentiment, '/doc/<string:doc_id>/sentiment')
 api.add_resource(SolrWiki, '/wiki/<string:doc_id>/solr')
