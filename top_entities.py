c485de1c27118761830f97c434cc7dfec6ac33f7
Robert Elwell <robert@wikia-inc.com>
2013-09-05 16:17:31 +0000

    test script for getting top entities from a wiki

---
+++ top_entities.py
@@ -0,0 +1,9 @@
+import sys
+from nlp_client import services
+
+try:
+    services.useCaching()
+    te = services.TopEntitiesService()
+    print te.get(sys.argv[1])
+except KeyboardInterrupt:
+    sys.exit()
