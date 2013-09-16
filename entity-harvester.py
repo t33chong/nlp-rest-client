6f689cb4edb33330fcf5a2d6ea257bb1a463fe55
Tristan Euan Chong <tristaneuan@gmail.com>
2013-09-16 23:00:06 +0000

    removed infinite retry loop and re-raised error

--- entity-harvester.py
+++ entity-harvester.py
@@ -11,16 +11,15 @@ from nlp_client.services import SolrWikiService, TopEntitiesService
 from _mysql_exceptions import OperationalError
 
 def main(wid):
-    while True:
-        try:
-            response = TopEntitiesService().get(wid)[wid]
-        except:
-            continue
-        break
-    entities = [entity[0].decode('utf-8') for entity in response]
-    url = SolrWikiService().get(int(wid))[int(wid)]['url'].decode('utf-8')
-    print url.encode('utf-8') + ',' + ','.join(entities).encode('utf-8')
-    Popen('python %s -w %s' % (os.path.join(os.getcwd(), 'purge-cassandra.py'), wid), shell=True)
+    try:
+        response = TopEntitiesService().get(wid)[wid]
+        entities = [entity[0].decode('utf-8') for entity in response]
+        url = SolrWikiService().get(int(wid))[int(wid)]['url'].decode('utf-8')
+        print url.encode('utf-8') + ',' + ','.join(entities).encode('utf-8')
+    except:
+        raise
+    finally:
+        Popen('python %s -w %s' % (os.path.join(os.getcwd(), 'purge-cassandra.py'), wid), shell=True)
 
 if __name__ == '__main__':
     wid = sys.argv[1]
