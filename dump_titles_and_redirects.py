c72695292e62a3f288ecc5914b576acd8c057623
Tristan Euan Chong <tristaneuan@gmail.com>
2013-10-08 19:50:39 +0000

    clean up logging

--- dump_titles_and_redirects.py
+++ dump_titles_and_redirects.py
@@ -17,16 +17,13 @@ from nlp_client.services import AllTitlesService, RedirectsService
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-file_handler = logging.FileHandler('dump_titles.log')
-file_handler.setLevel(logging.DEBUG)
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.ERROR)
+fh = logging.FileHandler('dump_titles.log')
+fh.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler.setFormatter(formatter)
+fh.setFormatter(formatter)
 
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
+logger.addHandler(fh)
 
 
 title_confirmation.USE_S3 = False
