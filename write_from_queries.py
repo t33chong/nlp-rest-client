e1bf7e6a1a985e730d13562944bf5df07aaa5428
Tristan Euan Chong <tristaneuan@gmail.com>
2013-10-12 00:06:01 +0000

    added multiprocessing

--- write_from_queries.py
+++ write_from_queries.py
@@ -6,12 +6,14 @@ query queue files.
 import os
 import sys
 import json
+import uuid
 import socket
 import shutil
 import logging
 import tarfile
 import requests
 from optparse import OptionParser
+from multiprocessing import Process, Queue
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 from utils import clean_list, ensure_dir_exists
@@ -40,13 +42,62 @@ WORKERS = options.workers
 LOCAL = options.local
 
 EVENT_DIR = '/data/events/'
-PROC_DIR = '/data/processing/'
+TEMP_EVENT_DIR = '/data/temp_events/'
 TEXT_DIR = '/data/text/'
-
-# List of query queue files to iterate over
-event_files = [os.path.join(EVENT_DIR, event_file) for event_file in os.listdir(EVENT_DIR)]
+TEMP_TEXT_DIR = '/data/temp_text/'
 
 def write_text(event_file):
+    doc_count = 0
     for line in open(event_file):
         query = line.strip()
-        
+        logger.info('query: "%s"' % query)
+        qi = QueryIterator('http://search-s11.prod.wikia.net:8983/solr/main/', {'query': query, 'fields': 'id,wid,html_en,indexed', 'sort': 'id asc'})
+        for doc in qi:
+            # sanitize and write text
+            text = '\n'.join(clean_list(doc.get('html_en', '')))
+            localpath = os.path.join(TEXT_DIR, doc['id'])
+            with open(localpath, 'w') as f:
+                f.write(text)
+            doc_count += 1
+    return doc_count
+
+def write_worker(event_queue, count_queue):
+    """Takes queue of event files as input, returns count of docs written as
+    output"""
+    try:
+        for event_file in iter(event_queue.get, None):
+            doc_count = write_text(event_file)
+            count_queue.put(doc_count)
+    except Exception, e:
+        count_queue.put(0)
+
+if __name__ == '__main__':
+    # List of query queue files to iterate over
+    event_files = [os.path.join(EVENT_DIR, event_file) for event_file in os.listdir(EVENT_DIR)]
+    event_queue = Queue()
+    count_queue = Queue()
+
+    for event_file in event_files:
+        event_queue.put(event_file)
+
+    write_workers = [Process(target=write_worker, args=(event_queue, count_queue)) for n in range(WORKERS)]
+
+    for write_worker in write_workers:
+        write_worker.start()
+
+    batch_count = 0
+    for count in iter(count_queue.get, None):
+        batch_count += count
+        if batch_count > 500:
+            text_files = [(os.path.join(TEXT_DIR, filename), os.path.getmtime(filename)) for filename in os.listdir(TEXT_DIR)]
+            text_files.sort(key=lambda x: x[1])
+            tempid = str(uuid.uuid4())
+            tempdir = os.path.join(TEXT_TEMP_DIR, tempid)
+            for text_file in text_files[:500]:
+                shutil.move(text_file, os.path.join(tempdir, os.path.basename(text_file)))
+            # tar batch
+            tar_file = text_dir + '.tgz'
+            tar = tarfile.open(tar_file, 'w:gz')
+            tar.add(text_dir, '.')
+            tar.close()
+            batch_count -= 500
