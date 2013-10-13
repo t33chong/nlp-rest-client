0670a58d75206808c147d142e4f4815db7534698
Robert Elwell <robert@wikia-inc.com>
2013-10-13 18:18:01 +0000

    prioritize unfinished wikis first

--- wiki_data_extraction.py
+++ wiki_data_extraction.py
@@ -24,12 +24,24 @@ def callServices(wid):
         print "Problem with", wid
         exc_type, exc_value, exc_traceback = sys.exc_info()
         print "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
-    
 
+def process(x):
+    if connect_s3().get_bucket('nlp-data').get_key('service_responses/%s/TopEntitiesService.get' % x) is not None:
+        print x, "already processed"
+    else:
+        print "Calling services on", x
+        callServices(x)
+    
+warmOnly = True
 while True:
     print "Getting wids"
     wids = [prefix.name.split('/')[-2] for prefix in connect_s3().get_bucket('nlp-data').list(prefix='xml/', delimiter='/') if isinstance(prefix, Prefix)]
     print "Working on %d wids" % len(wids)
     # shuffled to improve coverage across a pool
     random.shuffle(wids)
-    Pool(processes=4).map(callServices, wids)
+    if warmOnly:
+        Pool(processes=4).map(process, wids)
+    else:
+        Pool(processes=4).map(callServices, wids)
+    warmOnly = False
+    print "Finished"
