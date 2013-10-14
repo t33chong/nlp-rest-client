0b5abde255333507d9a58400eee27ffa972fc6fc
Robert Elwell <robert@wikia-inc.com>
2013-10-14 18:25:18 +0000

    switch to subprocess instead of multithreading

--- wiki_data_extraction.py
+++ wiki_data_extraction.py
@@ -1,37 +1,12 @@
-import requests
-import json
-import traceback
-import sys
-import os
 import random
+import sys
+from time import sleep
+from subprocess import Popen, STDOUT
 from boto import connect_s3
 from boto.s3.prefix import Prefix
-from multiprocessing import Pool
-from nlp_client.services import TopEntitiesService, EntityDocumentCountsService, TopHeadsService
-from nlp_client.caching import useCaching
 
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
-useCaching(writeOnly=True)
 
-def callServices(wid):
-    try:
-        TopEntitiesService().get(wid)
-        EntityDocumentCountsService().get(wid)
-        TopHeadsService().get(wid)
-        print wid
-        return 1
-    except:
-        print "Problem with", wid
-        exc_type, exc_value, exc_traceback = sys.exc_info()
-        print "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
-
-def process(x):
-    if connect_s3().get_bucket('nlp-data').get_key('service_responses/%s/TopEntitiesService.get' % x) is not None:
-        print x, "already processed"
-    else:
-        print "Calling services on", x
-        callServices(x)
-    
 warmOnly = True
 while True:
     print "Getting wids"
@@ -39,9 +14,15 @@ while True:
     print "Working on %d wids" % len(wids)
     # shuffled to improve coverage across a pool
     random.shuffle(wids)
-    if warmOnly:
-        Pool(processes=4).map(process, wids)
-    else:
-        Pool(processes=4).map(callServices, wids)
+    processes = []
+    
+    while len(wids) > 0:
+        while len(processes) < 4 and len(wids) > 0:
+            popen_params = ['python', 'wiki_data_extraction_child.py', wids.pop()]
+            if warmOnly:
+                popen_params += ['1']
+            processes += [Popen(popen_params, stdout=STDOUT, shell=True)]
+            processes = filter(lambda x: x.poll() is None, processes)
+        sleep(15)
     warmOnly = False
     print "Finished"
---
+++ wiki_data_extraction_child.py
@@ -0,0 +1,38 @@
+import sys
+import traceback
+import os
+from boto import connect_s3
+from nlp_client.services import TopEntitiesService, EntityDocumentCountsService, TopHeadsService
+from nlp_client.caching import useCaching
+
+sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
+useCaching(writeOnly=True)
+
+
+def callServices(wid):
+    try:
+        TopEntitiesService().get(wid)
+        EntityDocumentCountsService().get(wid)
+        TopHeadsService().get(wid)
+        print wid
+        return 1
+    except:
+        print "Problem with", wid
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        print "".join(traceback.format_exception(exc_type, exc_value, exc_traceback))
+
+
+def process(x):
+    if connect_s3().get_bucket('nlp-data').get_key('service_responses/%s/TopEntitiesService.get' % x) is not None:
+        print x, "already processed"
+    else:
+        print "Calling services on", x
+        callServices(x)
+
+
+wid = sys.argv[1]
+if len(sys.argv) > 2:
+    process(wid)
+else:
+    callServices(wid)
+
