9ba4f7f7c008e1e760d337a52898c1fc8237bbeb
Robert Elwell <robert@wikia-inc.com>
2013-10-21 15:56:28 +0000

    updated topwam

--- getTopWam.py
+++ getTopWam.py
@@ -1,10 +1,11 @@
-import requests
+import requests,sys
 from time import sleep
 from subprocess import Popen
 from random import shuffle
 
 r = requests.get('http://search-s10:8983/solr/xwiki/select', params={'wt':'json', 'q':'lang_s:en', 'sort': 'wam_i desc', 'rows':'10000', 'fl':'id'})
 ids = [doc['id'] for doc in r.json()['response']['docs']]
+
 shuffle(ids)
 processes = []
 while len(ids) > 1:
