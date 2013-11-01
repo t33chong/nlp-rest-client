c9c53f0f95546ea9d40e453303583f4e35c5078e
Robert Elwell <robert@wikia-inc.com>
2013-11-01 21:45:42 +0000

    changes for wikidata text mover to get wiki text pipeline working

--- wikidata-text-mover.py
+++ wikidata-text-mover.py
@@ -5,15 +5,18 @@ import sys
 if not os.path.exists('/data/wiki_xml/'):
     os.mkdir('/data/wiki_xml/')
 
-if len(sys) > 1:
+if len(sys.argv) > 1:
     print "Starting up and moving text to parser tmp"
-    shtuil.copytree('/data/wiki_text/', '/tmp/text/')
+    [shutil.copy('/data/wiki_text/%s' % fl, '/tmp/text/%s' % fl) for fl in os.listdir('/data/wiki_text/')]
 else:
     print "Skipping moving text over"
 
 while True:
     for fl in os.listdir('/tmp/xml/'):
-        wid, field = fl.split('_')
-        if not os.path.exists('/data/wiki_xml/%s' % wid):
-            os.mkdir('/data/wiki_xml/%s' % wid)
-        shutil.move('/tmp/xml/'+fl, '/tmp/wiki_xml/%s/%s' % (wid, field)))
+        spl = fl.split('_')
+        wid = spl[0]
+        field = "_".join(spl[1:])
+        wiki_xml_dir = '/data/wiki_xml/%s' % wid
+        if not os.path.exists(wiki_xml_dir):
+            os.mkdir(wiki_xml_dir)
+        shutil.move('/tmp/xml/'+fl, '%s/%s' % (wiki_xml_dir, field))
