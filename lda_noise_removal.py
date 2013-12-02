0854f94faee990b276e6b8fa6fe2464848d73d14
Tristan Euan Chong <tristaneuan@gmail.com>
2013-12-02 23:38:57 +0000

    clean up write format

--- lda_noise_removal.py
+++ lda_noise_removal.py
@@ -10,6 +10,7 @@ parser = OptionParser()
 parser.add_option('-i', '--input', dest='input_file', action='store', help='Location of CSV file from which to remove high-frequency features')
 parser.add_option('-o', '--output', dest='output_file', action='store', default=False, help='Location of CSV file to write to')
 parser.add_option('-m', '--max-freq', dest='maxfreq', action='store', default=500, help='Integer frequency above which a feature will be omitted')
+parser.add_option('-t', '--topics', dest='topics', action='store', default=999, help='Number of topics')
 options, args = parser.parse_args()
 
 if not options.output_file:
@@ -29,8 +30,6 @@ with open(options.input_file) as input_file:
     with open(options.output_file, 'w') as output_file:
         for line in input_file:
             data = line.strip().split(',')
-            output = data[0]
-            if len(data) > 1:
-                pairs = [pair if tally[int(pair.split('-')[0])] < options.maxfreq else '%s-%s' % (pair.split('-')[0], 'null') for pair in data[1:]]
-                output += ',' + ','.join(pairs)
-            output_file.write(output + '\n')
+            pairs = dict([(int(pair.split('-')[0]), float(pair.split('-')[1])) for pair in data[1:]])
+            output = ','.join([data[0]] + ['%d-%.8f' % (n, pairs.get(n, 0)) if tally[n] < options.maxfreq else '%d-%.8f' % (n, 0) for n in range(options.topics)]) + '\n'
+            output_file.write(output)
