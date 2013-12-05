4bc3493071c43ca224cb1559f92205a024607555
Tristan Euan Chong <tristaneuan@gmail.com>
2013-12-05 22:13:53 +0000

    pull entities from solr instead of TopEntitiesService

--- lda_clustering.py
+++ lda_clustering.py
@@ -1,8 +1,8 @@
 # Attempt to make sense of topic features by identifying the top entities
 # common to them across different wikis
 
-import json
 import logging
+import requests
 import sys
 import traceback
 from collections import defaultdict
@@ -10,6 +10,8 @@ from multiprocessing import Pool
 from nlp_client.caching import useCaching
 from nlp_client.services import TopEntitiesService
 from wiki_recommender import as_euclidean, get_topics_sorted_keys
+
+SOLR_URL = 'http://dev-search.prod.wikia.net:8983/solr/xwiki/select'
 
 useCaching(dontCompute=True)
 
@@ -26,12 +28,11 @@ def get_keys_and_entities(wid):
     """Given a wiki id, return a tuple containing:
     0. A list of keys (topics) for which term frequency is non-zero
     1. A cumulative list of top entities from all related wikis"""
-    entities = [entity for (entity, count) in TopEntitiesService().nestedGet(wid)]
+    entities = requests.get(SOLR_URL, params={'q': 'id:%s' % wid, 'fl': 'entities_txt', 'wt': 'json'}).json()['response']['docs'][0].get('entities_txt', [])
     doc, docs = as_euclidean(wid)
     related_wids = [wiki['id'] for wiki in docs]
     for related_wid in related_wids:
-        entities.extend([entity for (entity, count) in
-                         TopEntitiesService().nestedGet(wid)])
+        entities.extend(requests.get(SOLR_URL, params={'q': 'id:%s' % related_wid, 'fl': 'entities_txt', 'wt': 'json'}).json()['response']['docs'][0].get('entities_txt', []))
     keys = get_topics_sorted_keys(doc)
     return (keys, entities)
 
@@ -51,9 +52,11 @@ def cluster(wid):
 # Instantiate nested defaultdicts, with values of the inner defaulting to 0
 topics = defaultdict(lambda: defaultdict(int))
 
+count = 0
 # Iterate over top 5k wikis
 wids = [line.strip() for line in open('topwams.txt').readlines()[:5000]]
 for (keys, tally) in Pool(processes=8).map(cluster, wids):
+    print 'Count:', count
     # Increment topics dict w/ totals; keep running tally of entities per key
     for key in keys:
         for entity in tally:
@@ -63,4 +66,4 @@ for (keys, tally) in Pool(processes=8).map(cluster, wids):
 with open('clustered.csv', 'w') as f:
     for topic in topics:
         s = sorted(topics[topic].items(), key=lambda x: x[1], reverse=True)
-        f.write(','.join([topic] + [k for (k, v) in s[:25]]) + '\n')
+        f.write(','.join([topic] + [k for (k, v) in s[:25]]).encode('utf-8') + '\n')
