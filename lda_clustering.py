6658926685af43e1ccba6bd68c0cdb890cd49768
Tristan Euan Chong <tristaneuan@gmail.com>
2013-12-07 00:59:23 +0000

    included subjects and jaccard distance

--- lda_clustering.py
+++ lda_clustering.py
@@ -6,6 +6,8 @@ import requests
 import sys
 import traceback
 from collections import defaultdict
+from id_subject import preprocess
+from identify_wiki_subjects import identify_subject
 from multiprocessing import Pool
 from nlp_client.caching import useCaching
 from nlp_client.services import TopEntitiesService
@@ -24,17 +26,43 @@ sh = logging.StreamHandler()
 sh.setLevel(logging.INFO)
 log.addHandler(sh)
 
+# Jaccard functions taken from https://github.com/mouradmourafiq/data-analysis
+def jaccard_sim(tup_1, tup_2, verbose=False):
+    """
+        calculate the jaccard similiarity of 2 tuples
+    """
+    sum = len(tup_1) + len(tup_2)
+    set_1 = set(tup_1)
+    set_2 = set(tup_2)
+    inter = 0
+    for i in (set_1 & set_2):
+        count_1 = tup_1.count(i)
+        count_2 = tup_2.count(i)
+        inter += count_1 if count_1 < count_2 else count_2
+    j_sim = inter/sum
+    if verbose : print j_sim
+    return j_sim
+
+def jaccard_distance(tup_1, tup_2):
+    """
+        Calculate the jaccard distance
+    """
+    return 1 - jaccard_sim(tup_1, tup_2)
+
 def get_keys_and_entities(wid):
     """Given a wiki id, return a tuple containing:
     0. A list of keys (topics) for which term frequency is non-zero
     1. A cumulative list of top entities from all related wikis"""
     entities = requests.get(SOLR_URL, params={'q': 'id:%s' % wid, 'fl': 'entities_txt', 'wt': 'json'}).json()['response']['docs'][0].get('entities_txt', [])
+    subjects = identify_subject(wid, stemmed=True)
     doc, docs = as_euclidean(wid)
-    related_wids = [wiki['id'] for wiki in docs]
+    related_wids = [wiki['id'] for wiki in docs[:5]]
     for related_wid in related_wids:
         entities.extend(requests.get(SOLR_URL, params={'q': 'id:%s' % related_wid, 'fl': 'entities_txt', 'wt': 'json'}).json()['response']['docs'][0].get('entities_txt', []))
+        subjects.extend(identify_subject(related_wid, stemmed=True))
+    entities = list(set([preprocess(entity) for entity in entities]))
     keys = get_topics_sorted_keys(doc)
-    return (keys, entities)
+    return (keys, entities, subjects)
 
 def cluster(wid):
     log.info('Clustering wid ' + wid)
@@ -55,6 +83,11 @@ topics = defaultdict(lambda: defaultdict(int))
 count = 0
 # Iterate over top 5k wikis
 wids = [line.strip() for line in open('topwams.txt').readlines()[:5000]]
+
+#for wid in wids:
+#    print get_keys_and_entities(wid)
+#sys.exit(0)
+
 for (keys, tally) in Pool(processes=8).map(cluster, wids):
     print 'Count:', count
     # Increment topics dict w/ totals; keep running tally of entities per key
