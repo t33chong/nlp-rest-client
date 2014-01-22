d9fb350e2ee402a2622fed9f5de2a24629884e82
Tristan Euan Chong <tristaneuan@gmail.com>
2014-01-22 21:05:02 +0000

    updated AMI - avoid launching when queue is empty, fix poller IndexError

--- autoscale_monitor.py
+++ autoscale_monitor.py
@@ -12,7 +12,7 @@ We need this script for two reasons:
 COUNT = 0
 REGION = 'us-west-2'
 PRICE = '0.300'
-AMI = 'ami-d8debfe8'
+AMI = 'ami-a43c5c94'
 KEY = 'data-extraction'
 SECURITY_GROUPS = 'sshable'
 INSTANCE_TYPE = 'm2.4xlarge'
